890cea9acedc291fb55030c15d78c51756e46d2d
Sebastian Kulinski <sebastian.kulinski@codurance.com>
2023-08-24 12:31:57 +0200

    feat: moving midleware to it's own file

--- PythonFastApiBackend/main.py
+++ PythonFastApiBackend/main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI, Request
-from uuid import uuid4
-from starlette.middleware.base import BaseHTTPMiddleware
+from correlationId_middleware import CorrelationIdMiddleware
 from helloService import HelloWorldService
-from logger_config import logger, RequestState, ContextFilter  # Import from logger_config
+from logger_config import logger
 
 # No need to redefine ContextFilter, RequestState, and logger setup here
 
 app = FastAPI()
 
-class CorrelationIdMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        correlation_id = request.headers.get("X-Correlation-ID", str(uuid4()))
-        request.state.correlation_id = correlation_id
-        RequestState.correlation_id = correlation_id  # Use the RequestState from logger_config
-        response = await call_next(request)
-        response.headers["X-Correlation-ID"] = correlation_id
-        return response
+
 
 app.add_middleware(CorrelationIdMiddleware)
 hello_service = HelloWorldService()
