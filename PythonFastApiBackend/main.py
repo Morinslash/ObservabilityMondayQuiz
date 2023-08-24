bf3b1cdf7cddb32aa4a499b03942f824d6418178
Sebastian Kulinski <sebastian.kulinski@codurance.com>
2023-08-24 11:48:58 +0200

    feat: adding logging with external configuration

--- PythonFastApiBackend/main.py
+++ PythonFastApiBackend/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import logging
+
+logging.config.fileConfig('logging.ini')
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def read_root(request: Request):
+    logger.info(f"Received request from {request.client.host}")
     return {"Hello": "World"}
