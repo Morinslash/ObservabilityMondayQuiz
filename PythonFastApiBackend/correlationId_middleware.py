be0ef3d4c52d5fc40943c5248293c8666501fd06
Sebastian Kulinski <sebastian.kulinski@codurance.com>
2023-08-24 12:36:57 +0200

    logs formatted in json

--- PythonFastApiBackend/correlationId_middleware.py
+++ PythonFastApiBackend/correlationId_middleware.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import Request
 from uuid import uuid4
 from starlette.middleware.base import BaseHTTPMiddleware
 from logger_config import RequestState
@@ -7,7 +7,7 @@ class CorrelationIdMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         correlation_id = request.headers.get("X-Correlation-ID", str(uuid4()))
         request.state.correlation_id = correlation_id
-        RequestState.correlation_id = correlation_id  # Use the RequestState from logger_config
+        RequestState.correlation_id = correlation_id 
         response = await call_next(request)
         response.headers["X-Correlation-ID"] = correlation_id
         return response
--- PythonFastApiBackend/main.py
+++ PythonFastApiBackend/main.py
@@ -3,16 +3,12 @@ from correlationId_middleware import CorrelationIdMiddleware
 from helloService import HelloWorldService
 from logger_config import logger
 
-# No need to redefine ContextFilter, RequestState, and logger setup here
-
 app = FastAPI()
-
-
 
 app.add_middleware(CorrelationIdMiddleware)
 hello_service = HelloWorldService()
 
 @app.get("/")
 def read_root(request: Request):
-    logger.info(f"Received request from {request.client.host}")  # Use the logger from logger_config
+    logger.info(f"Received request from {request.client.host}")
     return hello_service.get_greeting()
