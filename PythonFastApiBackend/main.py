2f30da3b5b988127fed3f72997c4a5468f4a61d3
Sebastian Kulinski <sebastian.kulinski@codurance.com>
2023-08-24 12:09:10 +0200

    feat: working logger with correlationID from header or generate if not present

--- PythonFastApiBackend/main.py
+++ PythonFastApiBackend/main.py
@@ -1,10 +1,36 @@
 from fastapi import FastAPI, Request
 import logging
+from uuid import uuid4
+from starlette.middleware.base import BaseHTTPMiddleware
 
 logging.config.fileConfig('logging.ini')
 logger = logging.getLogger(__name__)
 
+
+class ContextFilter(logging.Filter):
+    def filter(self, record):
+        record.correlation_id = RequestState.correlation_id
+        return True
+
+# Define a global state for the request
+class RequestState:
+    correlation_id = ""
+
+# Add the filter to the logger
+logger.addFilter(ContextFilter())
+
 app = FastAPI()
+
+class CorrelationIdMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        correlation_id = request.headers.get("X-Correlation-ID", str(uuid4()))
+        request.state.correlation_id = correlation_id
+        RequestState.correlation_id = correlation_id  # Set the global state
+        response = await call_next(request)
+        response.headers["X-Correlation-ID"] = correlation_id
+        return response
+
+app.add_middleware(CorrelationIdMiddleware)
 
 @app.get("/")
 def read_root(request: Request):
